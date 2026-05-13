17feaf6412348a5c78d4b17374bd44463bbb8644
Adil-M2004 <D00265739@student.dkit.ie>
2026-05-13 17:25:42 +0100

    config updated

--- config.js
+++ config.js
@@ -4,10 +4,12 @@ const config = {
     firstName: process.env.firstName,
     lastName: process.env.lastName,
     employeeID: process.env.employeeID,
+    username: process.env.username,
+    password: process.env.passowrd,
     
     // Optional: Add a helper to ensure required variables are present
     checkEnv: () => {
-        const required = ['firstName', 'lastName', 'employeeID'];
+        const required = ['firstName', 'lastName', 'employeeID', 'username', 'password'];
         required.forEach(field => {
             if (!process.env[field]) {
                 console.warn(`Warning: Environment variable ${field} is missing!`);
